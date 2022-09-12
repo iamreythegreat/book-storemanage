982ba8262784c2ce5cedf50b472f441c65b43171
Rey <iamreythegreat@gmail.com>
2022-09-13 06:40:37 +0800

    'commit'

--- gologin.php
+++ gologin.php
@@ -2,13 +2,13 @@
 session_start();
 require __DIR__ . "/vendor/autoload.php";
 require __DIR__ . "/init.php";
-require __DIR__ . "/controllers/user.Controller.php";
-    
+require __DIR__ . "/controllers/user.Controller.php";   
 if ( $_POST['username'] && $_POST['username'] ) { 
         $user = new User($_POST['username']);
-        echo $user->login($_POST['password']);
-}
+        
+        if ( $user->login($_POST['password']) == 1 ) header("location: /dashboard.php");
 
+}
 header("location: /")
 
 ?>
